7fd1a5d3d5ac5f13775689c40954766b22c64c06
Doomzy <mohamed_abdulhamid5@outlook.com>
2024-06-27 18:59:27 +0200

    Installed & Configured Clerk package

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import Navbar from "./Navbar.jsx";
 import MobNav from "./MobNav.jsx";
 import DesktopNav from "./DesktopNav.jsx";
 import MenuBtn from "./MenuBtn.jsx";
+import ProtectedRoute from "./hoc/ProtectedRoute.jsx";
 
-export { Navbar, MobNav, DesktopNav, MenuBtn };
+export { Navbar, MobNav, DesktopNav, MenuBtn, ProtectedRoute };
