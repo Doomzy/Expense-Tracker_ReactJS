47797901b7f322191cb2a2fcce1ddd3489eb9d50
Doomzy <mohamed_abdulhamid5@outlook.com>
2024-07-14 13:45:56 +0200

    Applied the fetch items to the table component

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -6,7 +6,12 @@ function DashboardPage() {
     <div className="w-[90%] 2xl:w-3/4 m-auto mt-24">
       <TotalBalance />
       <SectionTitle text="My Transactions" link_to={""} />
-      <CustomTable data={fake_transactions} class_name=" md:w-full w-max" />
+      <CustomTable
+        data={fake_transactions}
+        class_name=" md:w-full w-max"
+        itemsPerPage={11}
+        enablePagination={true}
+      />
       <Modal display_type="block" />
     </div>
   );
