a85c3de841093ceb04c064085f288c64968c849d
Doomzy <mohamed_abdulhamid5@outlook.com>
2024-07-21 15:27:43 +0200

    Added the period part in tb componant & fixed datetime in table

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,25 +1,17 @@
-import { Money_Icon } from "../assets";
 import {
   NewTransactionCard,
   LatestEarnings,
   CustomTable,
   SectionTitle,
   Modal,
+  TotalBalance,
 } from "../components";
 import { latest_transaction } from "../constants";
 
 function HomePage() {
   return (
     <div className="w-[90%] 2xl:w-4/5 m-auto mt-24">
-      <div className="flex gap-4 box_shadow justify-center bg-white w-full lg:w-fit min-w-80 px-5 py-8 rounded-xl m-auto font-semibold text-primary-dark">
-        <div className="content-center">
-          <Money_Icon width={90} height={90} />
-        </div>
-        <div>
-          <p className=" text-xl">Total Balance</p>
-          <span className="text-6xl text-green">${1000000}</span>
-        </div>
-      </div>
+      <TotalBalance />
       <div className=" lg:grid grid-cols-3 gap-12 2xl:gap-22 mt-14">
         <NewTransactionCard extra_classes="lg:block hidden" />
         <div className=" col-span-2">
