b08215023d93be56a3a677143d5a84091ea7e1af
Doomzy <mohamed_abdulhamid5@outlook.com>
2024-07-26 14:38:31 +0200

    Created a store for the Modal component states

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -2,5 +2,12 @@ import useAddItem from "./useAddItem.js";
 import useGetItems from "./useGetItems.js";
 import useTotalBalanceStore from "./useTotalBalanceStore.js";
 import fetchTransactions from "./fetchTransactions.js";
+import useModalStore from "./useModalStore.js";
 
-export { useAddItem, useGetItems, useTotalBalanceStore, fetchTransactions };
+export {
+  useAddItem,
+  useGetItems,
+  useTotalBalanceStore,
+  fetchTransactions,
+  useModalStore,
+};
