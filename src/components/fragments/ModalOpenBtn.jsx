e34445dc2e933ee1bc3c7f7660e7f7e2e4675887
Doomzy <mohamed_abdulhamid5@outlook.com>
2024-08-21 16:04:19 +0200

    modified the modalOpenBtn & removed console.log lines

--- src/components/fragments/ModalOpenBtn.jsx
+++ src/components/fragments/ModalOpenBtn.jsx
@@ -1,13 +1,13 @@
 import { useModalStore } from "../../hooks";
 import { Plus_Icon } from "../../assets";
 
-function ModalOpenBtn() {
+function ModalOpenBtn({ display_classes = "block" }) {
   const isOpen = useModalStore((state) => state.isOpen);
   const handleOpen = useModalStore((state) => state.handleOpen);
 
   return (
     !isOpen && (
-      <div onClick={handleOpen}>
+      <div className={display_classes} onClick={handleOpen}>
         <button className="modal-open box_shadow">
           <Plus_Icon width={60} height={60} />
         </button>
